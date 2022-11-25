008ec6776c32b0fe9affe3e713c575c9155a78b3
Dani <git@danny.works>
2022-11-24 18:02:08 -0800

    Setsuzoku 0.3.1: Fix relative path for Setsuzoku infinity symbol

--- renderer/pages/_app.jsx
+++ renderer/pages/_app.jsx
@@ -15,7 +15,7 @@ function MyApp({ Component, pageProps }) {
             <div className="flex flex-col md:flex-row md:justify-between md:px-24 items-center text-5xl font-bold font-Inter w-full bg-slate-800 md:bg-transparent z-50 px-8 py-8 md:py-0 rounded-2xl md:rounded-none">
                 <div className="flex flex-row items-center text-5xl">
                     <CgInfinity className="hidden text-8xl text-blue bg-clip-text bg-gradient-to-r from-indigo-200 via-red-200 to-yellow-100" />
-                    <Image src="/images/infinity.webp" width={148} height={100} unoptimized />
+                    <Image src="images/infinity.webp" width={148} height={100} unoptimized />
                 </div>
                 <div className="flex flex-col space-y-6 items-center transition-all ease-in-out duration-250 text-xl">
                     <Component {...pageProps} />
